24f6587f00dbb0ae433de5c78d6025bde01cc7d9
Nicholas Hill <nicholas.hill@colorado.edu>
2019-10-01 09:23:59 -0600

    Updating. No significant changes.

--- tbx/cybox/bulkSeg.m
+++ tbx/cybox/bulkSeg.m
@@ -1,7 +1,7 @@
 function LL = bulkSeg(cellImage, opts)
-%BULKSEG is a simplified version of segmentation derived from
-%'nicksversion'. It should be used for single images of cells (from bulk
-%experiments), and not for movies.
+%BULKSEG is a simplified version of segmentation that should be used for
+%single images of cells (from bulk experiments), and not for movies. Uses
+%old threshold level (picks out ThLvl percentile of brightest pixels)
 
 
 %Pre-process the brightfield image: median filter and
@@ -51,14 +51,20 @@ maskCC = bwconncomp(mask);
 
 stdBG = std(cellImageTemp(~mask));
 
+
+stdObjs = []; %For analysis - not segmentation
 for iObj = 1:maskCC.NumObjects
     
     stdObj = std(cellImageTemp(maskCC.PixelIdxList{iObj}));
     
-    if stdObj < 5 * stdBG
+    stdObjs(end + 1) = stdObj; %For analysis - not segmentation
+    
+    if stdObj < 1500
         mask(maskCC.PixelIdxList{iObj}) = 0;
     end
 end
+
+% nhist(stdObjs) %For analysis - not segmentation
 
 
 %Redraw the masks using cylinders
@@ -68,70 +74,6 @@ rpCells = regionprops(mask,{'Centroid','MajorAxisLength','MinorAxisLength','Orie
 rpCells(([rpCells.Area] < min(opts.cellAreaLim)) | ([rpCells.Area] > max(opts.cellAreaLim))) = [];
 
 LL = CyTracker.drawCapsule(size(mask), rpCells);
-
-
-
-
-% % % % % % % % % keyboard
-% % % % % % % % % %Pre-process the brightfield image: median filter and
-% % % % % % % % % %background subtraction
-% % % % % % % % % cellImage = double(cellImage);
-% % % % % % % % % 
-% % % % % % % % % bgImage = imopen(cellImage, strel('disk', 40));
-% % % % % % % % % cellImageTemp = cellImage - bgImage;
-% % % % % % % % % cellImageTemp = imgaussfilt(cellImageTemp, 2);
-% % % % % % % % % 
-% % % % % % % % % %Fit the background
-% % % % % % % % % [nCnts, xBins] = histcounts(cellImageTemp(:), 100);
-% % % % % % % % % nCnts = smooth(nCnts, 3);
-% % % % % % % % % xBins = diff(xBins) + xBins(1:end-1);
-% % % % % % % % % 
-% % % % % % % % % %Find the peak background
-% % % % % % % % % [bgPk, bgPkLoc] = max(nCnts);
-% % % % % % % % % 
-% % % % % % % % % %Find point where counts drop to fraction of peak
-% % % % % % % % % thLoc = find(nCnts(bgPkLoc:end) <= bgPk * opts.thFactor, 1, 'first');
-% % % % % % % % % thLoc = thLoc + bgPkLoc;
-% % % % % % % % % thLvl = xBins(thLoc);
-% % % % % % % % % 
-% % % % % % % % % %Compute initial cell mask
-% % % % % % % % % mask = cellImageTemp > thLvl;
-% % % % % % % % % mask = imopen(mask, strel('disk', 3));
-% % % % % % % % % mask = imclose(mask, strel('disk', 3));
-% % % % % % % % % mask = imerode(mask, ones(1));
-% % % % % % % % % 
-% % % % % % % % % %Separate the cell clumps using watershedding
-% % % % % % % % % dd = -bwdist(~mask);
-% % % % % % % % % dd(~mask) = -Inf;
-% % % % % % % % % dd = imhmin(dd, opts.maxCellminDepth);
-% % % % % % % % % LL = watershed(dd);
-% % % % % % % % % mask(LL == 0) = 0;
-% % % % % % % % % 
-% % % % % % % % % %Tidy up
-% % % % % % % % % mask = imclearborder(mask);
-% % % % % % % % % mask = bwareaopen(mask, 100);
-% % % % % % % % % mask = bwmorph(mask,'thicken', 8);
-% % % % % % % % % 
-% % % % % % % % % %Filter out all objects with low StDev
-% % % % % % % % % maskCC = bwconncomp(mask);
-% % % % % % % % % keyboard
-% % % % % % % % % stdBG = std(cellImageTemp(~mask));
-% % % % % % % % % 
-% % % % % % % % % for iObj = 1:maskCC.NumObjects
-% % % % % % % % %     
-% % % % % % % % %     stdObj = std(cellImageTemp(maskCC.PixelIdxList{iObj}));
-% % % % % % % % %     
-% % % % % % % % %     if stdObj < 3 * stdBG
-% % % % % % % % %         mask(maskCC.PixelIdxList{iObj}) = 0;
-% % % % % % % % %     end
-% % % % % % % % % end
-% % % % % % % % % 
-% % % % % % % % % %Redraw mask as cylinders
-% % % % % % % % % rpCells = regionprops(mask,{'Centroid','MajorAxisLength','MinorAxisLength','Orientation','Area'});
-% % % % % % % % % LL = CyTracker.drawCapsule(size(mask), rpCells);
-
-
-
 
 end
 
